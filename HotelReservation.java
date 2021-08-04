430ac73cd87bdb587c9913cfc009652e367dcc46
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-04 08:52:29 +0530

    Update HotelReservation.java

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,5 +1,10 @@
 package HotelReservationProgram;
 
 public class HotelReservation {
+  
+   public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Program");
+    }
+  
 
 }
