b425e7c15cac78bdb872b9ece919d48bbdb9d08a
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-04 09:56:40 +0530

    Update HotelReservation.java

--- HotelReservation.java
+++ HotelReservation.java
@@ -2,7 +2,6 @@ package HotelReservationProgram;
 
 public class HotelReservation {
 	
-
 	// Method to add hotels details with name and rates for regular customer
 	public Hotel addHotel(String hotelName, int weekDayRate, int weekendRate) {
         return new Hotel(hotelName, weekDayRate, weekendRate);
